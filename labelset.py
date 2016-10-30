8903f2da55a17d11f7d32d2f1af0c9561b854b52
achan1989 <achan1989@gmail.com>
2016-10-30 17:25:49 +0000

    Specific exception classes for label problems, fix bug in add_generic().

--- labelset.py
+++ labelset.py
@@ -6,22 +6,29 @@ class LabelSet():
 
     def add(self, address, label):
         if address in self._address_labels:
-            raise Exception("{:#04X} already has the label {}".format(
-                address, self._address_labels[address]))
+            raise TargetRelabelException("{:#04X} has already been given the "
+                "label {}".format(address, self._address_labels[address]))
         if label in self._label_addresses:
-            raise Exception('The label "{}" is being used at {:#04X}'.format(
-                label, address))
+            raise LabelReuseException('The label "{}" is already used '
+                'at {:#04X}'.format(label, address))
 
         self._address_labels[address] = label
         self._label_addresses[label] = address
 
     def add_generic(self, address):
         label = "label{:04}".format(self._generic_id)
+        self.add(address, label)
         self._generic_id += 1
-        self.add_label(address, label)
 
     def get_label(self, address):
         return self._address_labels.get(address, None)
 
     def get_address(self, label):
         return self._label_addresses.get(label, None)
+
+
+class TargetRelabelException(Exception):
+    pass
+
+class LabelReuseException(Exception):
+    pass
