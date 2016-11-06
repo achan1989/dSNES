f0d0f7df783c9cd6da7e929f8572efcde92043e7
achan1989 <achan1989@gmail.com>
2016-11-06 20:17:17 +0000

    Resolve jump targets to labels when printing JMP and Bxx.

--- printing.py
+++ printing.py
@@ -57,22 +57,51 @@ def format_implicit(instruction, symbols=None):
     return "{ins}".format(ins=instruction.mnemonic)
 
 def format_jmp_absolute(instruction, symbols=None):
-    return "{ins} ${ops:04X}".format(
+    format_string = "{ins} ${address:04X}"
+    jmp_target = dw_to_uint(instruction.operands)
+    label = None
+
+    if symbols:
+        label = symbols.get_label(jmp_target)
+        if label:
+            format_string = "{ins} {label}"
+
+    return format_string.format(
         ins=instruction.mnemonic,
-        ops=dw_to_uint(instruction.operands))
+        address=jmp_target,
+        label=label)
 
 def format_jmp_absolute_indirect(instruction, symbols=None):
-    return "{ins} (${ops:04X})".format(
+    format_string = "{ins} (${address:04X})"
+    ind_target = dw_to_uint(instruction.operands)
+    label = None
+
+    if symbols:
+        label = symbols.get_label(ind_target)
+        if label:
+            format_string = "{ins} ({label})"
+
+    return format_string.format(
         ins=instruction.mnemonic,
-        ops=dw_to_uint(instruction.operands))
+        address=ind_target,
+        label=label)
 
 def format_relative(instruction, symbols=None):
-    value = tc_to_int(instruction.operands[0])
-    sign = "+" if value >= 0 else "-"
-    return "{ins} {sign}${ops:02X}".format(
+    format_string = "{ins} ${offset:+02X}"
+    offset = tc_to_int(instruction.operands[0])
+    label = None
+
+    if symbols:
+        relative_base = instruction.address + instruction.size
+        target = relative_base + offset
+        label = symbols.get_label(target)
+        if label:
+            format_string = "{ins} {label}"
+
+    return format_string.format(
         ins=instruction.mnemonic,
-        sign=sign,
-        ops=value)
+        offset=offset,
+        label=label)
 
 def format_ret(instruction, symbols=None):
     return "{ins}".format(ins=instruction.mnemonic)
