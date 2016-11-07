80b9a92fb116412bbab16231999bcb1f30318c9d
achan1989 <achan1989@gmail.com>
2016-11-07 20:49:32 +0000

    Normalise addresses when dealing with symbols.

--- symbolset.py
+++ symbolset.py
@@ -1,3 +1,6 @@
+import memory
+
+
 class SymbolSet():
     def __init__(self):
         self._address_labels = dict()
@@ -5,6 +8,7 @@ class SymbolSet():
         self._generic_id = 0
 
     def add_label(self, address, label):
+        address = memory.normalise(address)
         if address in self._address_labels:
             raise TargetRelabelException("{:#04X} has already been given the "
                 "label {}".format(address, self._address_labels[address]))
@@ -16,11 +20,20 @@ class SymbolSet():
         self._label_addresses[label] = address
 
     def add_generic_label(self, address):
+        address = memory.normalise(address)
         label = "label{:04}".format(self._generic_id)
         self.add_label(address, label)
         self._generic_id += 1
 
+    def add_generic_variable(self, address):
+        address = memory.normalise(address)
+        assert memory.RAM.contains(address)
+        label = "var{:04}".format(self._generic_id)
+        self.add_label(address, label)
+        self._generic_id += 1
+
     def get_label(self, address):
+        address = memory.normalise(address)
         return self._address_labels.get(address, None)
 
     def get_address(self, label):
