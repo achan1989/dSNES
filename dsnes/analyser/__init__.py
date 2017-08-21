435d072fc6b3fc0ccdf24e68997428bfdac27086
achan1989 <achan1989@gmail.com>
2017-08-21 22:40:43 +0100

    Analyser makes use of CPU state.

--- dsnes/analyser/__init__.py
+++ dsnes/analyser/__init__.py
@@ -74,12 +74,25 @@ class Analyser:
         bus = self.project.bus
         db = self.project.database
         orig_addr = address
+        # By default we don't know what state the CPU is in.
+        calculated_state = (None, None, None)
 
         while True:
-            s = self.state
+            # If the user claims to know the state for this instruction, use it.
+            declared_state = db.get_state(address)
+            if declared_state is not None:
+                self.state = declared_state
+                s = self.state
+            # Otherwise use the state that was calculated by executing the
+            # previous instruction.
+            else:
+                s = self.state
+                s.e, s.m, s.x = calculated_state
+
             disassembly = dsnes.disassemble(address, bus, s.e, s.m, s.x)
             self.disassembly.append(disassembly)
             address = disassembly.next_addr
+            calculated_state = disassembly.new_state
 
     @staticmethod
     def parse_state(s):
