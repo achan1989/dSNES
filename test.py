9526491b15c1a0816fdaff24c6a3824be4e37a8d
achan1989 <achan1989@gmail.com>
2017-08-28 13:36:30 +0100

    Add a debug command to the test interface: profile the project load function.

--- test.py
+++ test.py
@@ -8,13 +8,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--address", default=0xff9c, type=partial(int, base=0))
 parser.add_argument("--state", default=None)
 parser.add_argument("--stop-before", default=None, type=partial(int, base=0))
+parser.add_argument("--profile-load", action="store_true")
 args = parser.parse_args()
 
-project = dsnes.project.load("starfox")
-analyser = dsnes.Analyser(project)
-try:
-    analyser.analyse_function(args.address, args.state, args.stop_before)
-except:
-    analyser.display()
-    print("Processed {} instructions".format(len(analyser.visited)))
-    raise
+if args.profile_load:
+    import cProfile
+    import pstats
+    profile = cProfile.Profile()
+    profile.runcall(dsnes.project.load, "starfox")
+    stats = pstats.Stats(profile).sort_stats("cumtime")
+
+else:
+    project = dsnes.project.load("starfox")
+    analyser = dsnes.Analyser(project)
+    try:
+        analyser.analyse_function(args.address, args.state, args.stop_before)
+    except:
+        analyser.display()
+        print("Processed {} instructions".format(len(analyser.visited)))
+        raise
