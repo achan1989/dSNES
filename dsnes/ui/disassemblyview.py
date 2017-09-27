8ca57d56eae2d145f2929a8642f983f3183d2d91
achan1989 <achan1989@gmail.com>
2017-09-27 22:25:14 +0100

    Disassembly view has scrollbars, auto column width, no headings.

--- dsnes/ui/disassemblyview.py
+++ dsnes/ui/disassemblyview.py
@@ -6,30 +6,57 @@ from tkinter import font as tkfont
 from tkinter import ttk
 
 
-class DisassemblyView(ttk.Treeview):
-    def __init__(self, master=None, **kwargs):
-        super().__init__(master, **kwargs)
+class DisassemblyView(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
 
+        self.dasm = dasm = ttk.Treeview(self)
+        dasm.grid(column=0, row=0, sticky="nsew")
         columns = ("address", "raw", "asm", "guide", "comment", "state")
-        self["columns"] = columns
-        self["show"] = "headings"
-        self["selectmode"] = "browse"
-        self["padding"] = 0
+        dasm["columns"] = columns
+        dasm["show"] = "tree"  # Hides column headers.
+        dasm["selectmode"] = "browse"
+        dasm["padding"] = 0
 
         style = ttk.Style()
-        self.font = tkfont.Font(font="TkFixedFont", size=12)
+        self.font = tkfont.Font(name="DasmFont", font="TkFixedFont", size=12)
         style.configure("Monospace.Treeview", font=self.font)
-        self["style"] = "Monospace.Treeview"
+        dasm["style"] = "Monospace.Treeview"
 
+        # Hide the empty "item" column.
+        dasm.column("#0", width=0, stretch=False)
+        # Columns stay at their given size, except for the comment, which
+        # changes based on the size of the window.
         for col in columns:
-            self.column(col, stretch=True)
-            self.heading(col, text=col)
+            dasm.column(col, stretch=False, width=1)
+            dasm.heading(col, text=col)
+        dasm.column("comment", stretch=True)
 
-        self.insert("", "end", values=["1f:bdb1", ":22 63 f9 1f", "jsl $123456", "[[some_label]]", "; Inline comment goes here, can be long", "p=mxe b=0 d=0"])
+        vscroll = ttk.Scrollbar(self, orient="vertical", command=dasm.yview)
+        vscroll.grid(column=1, row=0, sticky="ns")
+        dasm['yscrollcommand'] = vscroll.set
 
-        self.bind("<Button-1>", self.handle_click)
+        hscroll = ttk.Scrollbar(self, orient="horizontal", command=dasm.xview)
+        hscroll.grid(column=0, row=1, sticky="ew")
+        dasm['xscrollcommand'] = hscroll.set
 
-    def handle_click(self, evt):
+        example_item = ["1f:bdb1", ":22 63 f9 1f", "jsl $123456", "[[some_label]]", "; Inline comment goes here, can be long", "p=mxe b=0 d=0"]
+        dasm.insert("", "end", values=example_item)
+
+        # Set column width to fit the width of the text.
+        # TODO: re-visit this when populating with real data.
+        # TODO: need to set the columns to the widest across all items.
+        # TODO: needs padding.
+        for idx, txt in enumerate(example_item):
+            width = self.font.measure(txt)
+            if dasm.column(idx, option="width") < width:
+                dasm.column(idx, width=width, minwidth=width)
+
+        dasm.bind("<Button-1>", self.handle_dasm_click)
+
+    def handle_dasm_click(self, evt):
         # Prevent resizing columns.
-        if self.identify_region(evt.x, evt.y) == "separator":
+        if self.dasm.identify_region(evt.x, evt.y) == "separator":
             return "break"
