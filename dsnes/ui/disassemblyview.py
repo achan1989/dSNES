590ae65a55591d60f51d2ae4faf6b0aea76c4fa8
achan1989 <achan1989@gmail.com>
2017-10-01 15:07:27 +0100

    Populate the disassembly view when a new analysis is chosen.

--- dsnes/ui/disassemblyview.py
+++ dsnes/ui/disassemblyview.py
@@ -17,25 +17,17 @@ class DisassemblyView(ttk.Frame):
 
         self.dasm = dasm = ttk.Treeview(self)
         dasm.grid(column=0, row=0, sticky="nsew")
-        columns = ("address", "raw", "asm", "guide", "comment", "state")
-        dasm["columns"] = columns
         dasm["show"] = "tree"  # Hides column headers.
         dasm["selectmode"] = "browse"
         dasm["padding"] = 0
+
+        # Map displayed item/index to actual item/index (for multi-line items).
+        self.item_lookup = {}
 
         style = ttk.Style()
         self.font = tkfont.Font(name="DasmFont", font="TkFixedFont", size=12)
         style.configure("Monospace.Treeview", font=self.font)
         dasm["style"] = "Monospace.Treeview"
-
-        # Hide the empty "item" column.
-        dasm.column("#0", width=0, stretch=False)
-        # Columns stay at their given size, except for the comment, which
-        # changes based on the size of the window.
-        for col in columns:
-            dasm.column(col, stretch=False, width=1)
-            dasm.heading(col, text=col)
-        dasm.column("comment", stretch=True)
 
         vscroll = ttk.Scrollbar(self, orient="vertical", command=dasm.yview)
         vscroll.grid(column=1, row=0, sticky="ns")
@@ -44,18 +36,6 @@ class DisassemblyView(ttk.Frame):
         hscroll = ttk.Scrollbar(self, orient="horizontal", command=dasm.xview)
         hscroll.grid(column=0, row=1, sticky="ew")
         dasm['xscrollcommand'] = hscroll.set
-
-        example_item = ["1f:bdb1", ":22 63 f9 1f", "jsl $123456", "[[some_label]]", "; Inline comment goes here, can be long", "p=mxe b=0 d=0"]
-        dasm.insert("", "end", values=example_item)
-
-        # Set column width to fit the width of the text.
-        # TODO: re-visit this when populating with real data.
-        # TODO: need to set the columns to the widest across all items.
-        # TODO: needs padding.
-        for idx, txt in enumerate(example_item):
-            width = self.font.measure(txt)
-            if dasm.column(idx, option="width") < width:
-                dasm.column(idx, width=width, minwidth=width)
 
         dasm.bind("<Button-1>", self.handle_dasm_click)
         app.root.bind(
@@ -68,8 +48,70 @@ class DisassemblyView(ttk.Frame):
 
     def handle_analysis_updated(self, *args):
         print(events.ANALYSIS_UPDATED)
-        self.dasm.delete(self.dasm.get_children())
+        dasm = self.dasm
+        old_items = self.dasm.get_children()
+        if old_items:
+            dasm.delete(*old_items)
+        self.item_lookup.clear()
         dasm_lines = self.app.session.current_analysis.get_disassembly_lines()
+        curr_idx = self.app.session.line_number
 
-        for item in dasm_lines:
-            print(item)
+        for idx, item in enumerate(dasm_lines):
+            self.add_item(item, idx, idx==curr_idx)
+
+    def add_item(self, item, orig_index, is_selected):
+        kind = item.kind
+        if kind == "label":
+            self.add_label(item, orig_index)
+        elif kind == "pre-comment":
+            self.add_pre_comment(item, orig_index)
+        elif kind == "error":
+            self.add_error(item, orig_index)
+        elif kind == "disassembly":
+            self.add_disassembly(item, orig_index)
+        else:
+            raise ValueError("Unexpected item kind {!r}".format(kind))
+
+    def add_label(self, item, orig_index):
+        text = item.text + ":"
+        identity = self.dasm.insert(parent="", index="end", text=text)
+        display_index = self.dasm.index(identity)
+        self.item_lookup[display_index] = (orig_index, item)
+
+    def add_pre_comment(self, item, orig_index):
+        lines = item.text.split("\n")
+        for line in lines:
+            text = " " + line
+            identity = self.dasm.insert(parent="", index="end", text=text)
+            display_index = self.dasm.index(identity)
+            self.item_lookup[display_index] = (orig_index, item)
+
+    def add_error(self, item, orig_index):
+        error_msg = "!!!{}!!!".format(item.msg)
+        text = " {addr}{pad:<12}  {msg:^73}   {state}".format(
+            addr=format_address(item.addr),
+            pad="",
+            msg=error_msg,
+            state=item.state.encode())
+        identity = self.dasm.insert(parent="", index="end", text=text)
+        display_index = self.dasm.index(identity)
+        self.item_lookup[display_index] = (orig_index, item)
+
+    def add_disassembly(self, item, orig_index):
+        text = " {addr}:{raw:<11}  {asm:<15s}   {target:<18s}  {comment:<35s}   {state}".format(
+            addr=format_address(item.operation.addr),
+            raw=" ".join([format(n, "02x") for n in item.operation.raw]),
+            asm=item.operation.asm_str,
+            target=item.target_str,
+            comment=item.comment,
+            state=item.operation.state.encode())
+        identity = self.dasm.insert(parent="", index="end", text=text)
+        display_index = self.dasm.index(identity)
+        self.item_lookup[display_index] = (orig_index, item)
+
+def format_address(addr):
+    if addr < 0 or addr > 0xFFFFFF:
+        raise ValueError("Address out of range")
+    pbr = (addr & 0xff0000) >> 16
+    pc = addr & 0xFFFF
+    return "{:02x}:{:04x}".format(pbr, pc)
